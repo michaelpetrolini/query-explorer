165e44c1d7b701b6241d92cc6b994aeb5264e3c5
michaelpetrolini <petrolinimichael95@gmail.com>
2023-03-23 09:52:59 +0100

    rename unit tests

--- unit_test.py
+++ unit_test.py
@@ -5,43 +5,42 @@ from utility import get_tree, get_column, distance_from_ancestor
 
 class TestQuery(unittest.TestCase):
     def test_simple(self):
-        tree = get_tree(1)
+        tree = get_tree('simple')
         self.assertEqual(tree.graph.number_of_nodes(), 4)
         self.assertEqual(tree.graph.number_of_edges(), 0)
 
     def test_table_prefix(self):
-        tree = get_tree(2)
+        tree = get_tree('table_prefix')
         self.assertEqual(tree.graph.number_of_nodes(), 4)
         self.assertEqual(tree.graph.number_of_edges(), 0)
 
     def test_column_alias(self):
-        tree = get_tree(3)
+        tree = get_tree('column_alias')
         self.assertEqual(tree.graph.number_of_nodes(), 4)
         self.assertEqual(tree.graph.number_of_edges(), 0)
 
     def test_prefix_alias(self):
-        tree = get_tree(4)
+        tree = get_tree('prefix_alias')
         self.assertEqual(tree.graph.number_of_nodes(), 4)
         self.assertEqual(tree.graph.number_of_edges(), 0)
 
-    def test_dependency1(self):
-        tree = get_tree(5)
+    def test_dependency(self):
+        tree = get_tree('dependency')
         self.assertEqual(tree.graph.number_of_nodes(), 12)
         self.assertEqual(tree.graph.number_of_edges(), 5)
         self.assertEqual(distance_from_ancestor(tree, get_column(tree, 'c_surname')), 1)
 
-    def test_dependency2(self):
-        tree = get_tree(6)
+    def test_long_dependency(self):
+        tree = get_tree('long_dependency')
         self.assertEqual(tree.graph.number_of_nodes(), 18)
         self.assertEqual(tree.graph.number_of_edges(), 8)
         self.assertEqual(distance_from_ancestor(tree, get_column(tree, 'cost')), 2)
 
-
-def test_dependency3(self):
-    tree = get_tree(7)
-    self.assertEqual(tree.graph.number_of_nodes(), 12)
-    self.assertEqual(tree.graph.number_of_edges(), 5)
-    self.assertEqual(len(tree.graph.in_edges(get_column(tree, 'name_surname'))), 2)
+    def test_double_dependency(self):
+        tree = get_tree('double_dependency')
+        self.assertEqual(tree.graph.number_of_nodes(), 11)
+        self.assertEqual(tree.graph.number_of_edges(), 5)
+        self.assertEqual(len(tree.graph.in_edges(get_column(tree, 'name_surname'))), 2)
 
 
 if __name__ == '__main__':
